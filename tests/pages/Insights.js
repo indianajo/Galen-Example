e6891d9391ec070f82e95a40ae5f634e228a8661
indianajo <indianajo@users.noreply.github.com>
2020-09-12 14:01:48 +0200

    added accept cookies function

--- tests/pages/Insights.js
+++ tests/pages/Insights.js
@@ -1,8 +1,14 @@
 
 this.Insights = $page("Insights", {
     articleContainer: "#flowing-insights-articles-container",
-    loadMoreButton: "#show-more-insights div div a"
+    loadMoreButton: "#show-more-insights div div a",
+    acceptCookiesButton: "#onetrust-accept-btn-handler"
 }, {
+    acceptCookies: loggedFunction("Accept cookies if needed", function () {
+        Thread.sleep(1000);
+        this.acceptCookiesButton.click();
+    })
+,
     loadMoreResults: loggedFunction("Click load more button to show more article", function(){
         this.loadMoreButton.click();
     })
