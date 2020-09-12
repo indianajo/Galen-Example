9c7a600cd688fb2f49245bc70ba9f5f580a2bdf9
indianajo <indianajo@users.noreply.github.com>
2020-09-12 14:02:35 +0200

    accept cookies before running LayoutCheck

--- tests/insights.test.js
+++ tests/insights.test.js
@@ -3,6 +3,7 @@ load("pages/Insights.js");
 
 testOnAllDevices("Insights test", "/insights/software-testing", function (driver, device) {
     insights = new Insights(driver).waitForIt();
+    insights.acceptCookies();
     checkLayout(driver, "specs/insights.gspec", device.tags, device.excludedTags);
     insights.loadMoreResults();
     
